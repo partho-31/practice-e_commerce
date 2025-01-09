a978a7cab79899fbd3b9b1cb2f3cefe9987c5ab6
Partho <parthoemperors@gmail.com>
2025-01-09 11:01:56 +0600

    Added dynamic url

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('menu/',menu),
+    path('next/<int : id>',next),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def menu(response):
+def menu(request):
     food_list = ['Apple','Bannana','Jack fruits','Orange']
     return HttpResponse(food_list)
+
+def next(request,id):
+    return HttpResponse(f'this is a dynamic page {id}')
